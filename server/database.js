2f7e8f195994388b6ca4a5bddeb6ee3954007f39
Victor <victorkrook96@gmail.com>
2021-03-26 16:32:09 +0100

    First draft of Database and server done. Base functionality ok

---
+++ server/database.js
@@ -0,0 +1,97 @@
+const mysql = require('mysql');
+
+
+class Database {
+
+    constructor(config) {
+        this.config = config;
+        this.con = mysql.createConnection(config);
+    }
+
+    connect() {
+        this.con.connect(err => {
+            if (err) {
+                console.log('ERROR: Failed to connect to database');
+                throw err;
+            } 
+            console.log(`Connected to database ${this.config.database} at ${this.config.host}`);
+        })
+    }
+
+    close() {
+        this.con.end();
+    }
+
+    baseQuery(query, values, callback) {
+        this.con.query(query, values, (err, res) => {
+            let result;
+            if (err) {
+                console.log('ERROR Executing query!');
+                console.log(err)
+                result = null;
+            } else {
+                result = res;
+            }
+            if (callback != undefined)
+                callback(result);
+            else
+                console.log(result);
+        })
+    }
+
+    stringifyResult(res) {
+        if (res == null)
+            return {result: "error"};
+        
+        return res.affectedRows > 0 ? {result: "ok"} : {result: "no change"}
+    }
+
+    getTasks(callback) {
+        this.baseQuery('SELECT * from Task;', [], callback);
+    }
+
+    getLists(callback) {
+        this.baseQuery('SELECT * from List;', [], callback);
+    }
+
+    addTask(title, description, listId, callback) {
+        this.baseQuery('INSERT INTO Task (title, description, listId) ' + 
+                       'VALUES (?, ?, ?);',
+                       [title, description, listId], 
+                       res => callback(this.stringifyResult(res))
+                       );
+    }
+
+    addList(name, callback) {
+        this.baseQuery('INSERT INTO List (name) VALUES (?);', 
+                       [name],
+                       res => callback(this.stringifyResult(res)));
+    }
+
+    editTask(taskId, title, description, listId, callback) {
+        this.baseQuery('UPDATE Task SET title = ?, description = ?, listId = ? ' + 
+                       'WHERE taskId = ?;',
+                       [title, description, listId, taskId], 
+                       res => callback(this.stringifyResult(res)));
+    }
+
+    editList(listId, name, callback) {
+        this.baseQuery('UPDATE List SET name = ? WHERE listId = ?;',
+                       [name, listId], 
+                       res => callback(this.stringifyResult(res)));
+    }
+
+    deleteTask(taskId, callback) {
+        this.baseQuery('DELETE FROM Task WHERE taskId = ?;', [taskId],
+                        res => callback(this.stringifyResult(res)));
+    }
+
+    deleteList(listId, callback) {
+        this.baseQuery('DELETE FROM List WHERE listId = ?;', [listId],
+                        res => callback(this.stringifyResult(res)));
+    }
+
+}
+
+
+module.exports = Database;
---
+++ server/server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+// Read config file
+const fs = require('fs');
+const CONFIG_FILE = 'credentials.json';
+const CONFIG_FILE_PATH = __dirname + '/' + CONFIG_FILE;
+const config = JSON.parse(fs.readFileSync(CONFIG_FILE_PATH));
+
+// Start connection to database
+const Database = require('./database');
+const db = new Database(config.database);
+
+db.connect();
+
+app.listen(config.server.port, () => console.log(`Server stared at port ${config.server.port}`));
+
+app.route('/task')
+   .get((req, res) => {
+        db.getTasks(tasks => res.json(tasks));
+    })
+   .post((req, res) => {
+        db.addTask(req.query.title,
+                   req.query.description,
+                   req.query.listId,
+                   result => res.json(result));
+    })
+   .put((req, res) => {
+        db.editTask(req.query.taskId,
+                    req.query.title,
+                    req.query.description,
+                    req.query.listId,
+                    result => res.json(result));
+    })
+   .delete((req, res) => {
+        db.deleteTask(req.query.taskId,
+                      result => res.json(result));
+    })
+
+app.route('/list')
+    .get((req, res) => {
+        db.getLists(lists => res.json(lists));
+    })
+    .post((req, res) => {
+        db.addList(req.query.name,
+                   result => res.json(result));
+    })
+    .put((req, res) => {
+        db.editList(req.query.listId,
+                    req.query.name,
+                    result => res.json(result));
+    })
+    .delete((req, res) => {
+        db.deleteList(req.query.listId,
+                      result => res.json(result));
+    })
